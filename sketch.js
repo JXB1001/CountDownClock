00230dbde5a057d638e6ea90a98639f1f16c6acb
JXB1001 <bentley.silas@gmail.com>
2019-04-26 22:34:13 +0100

    First Iteration of A Countdown Clock

---
+++ sketch.js
@@ -0,0 +1,83 @@
+var data = [
+    {
+        name:"Small Embedded Systems Submission",
+        month:5,
+        day:9,
+        hour:23,
+        minute:59
+    },
+    {
+        name:"CCN Exam",
+        month:5,
+        day:21,
+        hour:9,
+        minute:30
+    },
+    {
+        name:"SCAS Exam",
+        month:5,
+        day:23,
+        hour:14,
+        minute:0
+    }
+]
+
+var daysInMonth = {1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:31};
+
+var c = {x:window.innerWidth*0.98, y:window.innerHeight*0.97};
+
+var currentDataInt;
+
+function setup() {
+    createCanvas(c.x,c.y);
+    currentDataInt = 0;
+    textAlign(CENTER);
+    fill(255);
+}
+
+function draw() {
+    background(0);
+    d = data[currentDataInt];
+    let daysToGo = 0
+    
+    if(d["month"] != month()){
+        daysToGo += daysInMonth[month()] - day();
+        tempMonth = month();
+        while(tempMonth < d.month-1){
+            tempMonth++;
+            daysToGo += daysInMonth[tempMonth];
+        }
+    }
+    daysToGo += d["day"];
+
+    textSize(60);
+    let hoursToGo = d["hour"]-hour();
+    let minutesToGo = d["minute"]-minute();
+    let secondsToGo = second();
+
+    hoursToGo = (hoursToGo < 0) ? hoursToGo = 23-hour()+d["hour"] : hoursToGo;
+    minutesToGo = (minutesToGo < 0) ? minutesToGo = 59-hour()+d["hour"] : minutesToGo;
+
+    write(daysToGo, "day", c.x/2, c.y/4)
+    write(hoursToGo, "hour", c.x/2, c.y/4 + 75*1)
+    write(minutesToGo, "minute", c.x/2, c.y/4 + 75*2)
+    write(secondsToGo, "second", c.x/2, c.y/4 + 75*3)
+
+    text(d["name"], c.x/2, c.y-c.y/4)
+}
+
+function write(v, tag, x, y){
+    var end = "";
+    var string = "";
+    if(v > 1){
+        end = "s";
+    }
+    string += v.toString();
+    string += (" " + tag + end);
+    text(string,x,y);
+}
+
+function mousePressed() {
+    currentDataInt = (currentDataInt+1)%data.length;
+}
+
