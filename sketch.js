dd63ee688fb82d85ba322a2666961b1c3998ae09
JXB1001 <bentley.silas@gmail.com>
2019-04-27 12:34:11 +0100

    Made the page more scalable depending of window size and added warning function when deadline is close

--- sketch.js
+++ sketch.js
@@ -27,16 +27,22 @@ var daysInMonth = {1:31,2:28,3:31,4:30,5:31,6:30,7:31,8:31,9:30,10:31,11:30,12:3
 var c = {x:window.innerWidth*0.98, y:window.innerHeight*0.97};
 
 var currentDataInt;
+const warningColour = "#FF0000";
+const normalColour = "#000000";
+var warning;
 
 function setup() {
     createCanvas(c.x,c.y);
     currentDataInt = 0;
     textAlign(CENTER);
     fill(255);
+    warning = false;
 }
 
 function draw() {
-    background(0);
+    
+    c = {x:window.innerWidth*0.98, y:window.innerHeight*0.97};
+    createCanvas(c.x,c.y);
     d = data[currentDataInt];
     let daysToGo = 0
     
@@ -50,7 +56,8 @@ function draw() {
     }
     daysToGo += d["day"];
 
-    textSize(60);
+    let ts = 60;
+    textSize(ts);
     let hoursToGo = d["hour"]-hour();
     let minutesToGo = d["minute"]-minute();
     let secondsToGo = second();
@@ -58,12 +65,25 @@ function draw() {
     hoursToGo = (hoursToGo < 0) ? hoursToGo = 23-hour()+d["hour"] : hoursToGo;
     minutesToGo = (minutesToGo < 0) ? minutesToGo = 59-hour()+d["hour"] : minutesToGo;
 
+    if(daysToGo < 2){
+        background(warningColour);
+    }
+    else{
+        background(normalColour);
+    }
+
     write(daysToGo, "day", c.x/2, c.y/4)
     write(hoursToGo, "hour", c.x/2, c.y/4 + 75*1)
     write(minutesToGo, "minute", c.x/2, c.y/4 + 75*2)
     write(secondsToGo, "second", c.x/2, c.y/4 + 75*3)
 
+    while(textWidth(d["name"]) > c.x*0.8){
+        ts--;
+        textSize(ts);
+    }
     text(d["name"], c.x/2, c.y-c.y/4)
+
+
 }
 
 function write(v, tag, x, y){
